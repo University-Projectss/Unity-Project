38d678cbad630992cdd3cb5b4c3a79584a46c551
Andrei Tava <tavaandrei@gmail.com>
2023-10-30 08:59:21 +0200

    implement camera lock on the Z axis

--- Flight Game/Assets/Scripts/Plane Physics/CameraRestrict.cs
+++ Flight Game/Assets/Scripts/Plane Physics/CameraRestrict.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class CameraRestrict : MonoBehaviour
 {
+    //the previous value of the parent's Z euler Angle
+    private float _previousZ;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _previousZ = transform.parent.localEulerAngles.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        var rotation = transform.rotation.eulerAngles;
-        transform.InverseTransformPoint(transform.position);
+        //We compute the rotation on the Z axis since the last frame
+        var deltaZ = transform.parent.localEulerAngles.z - _previousZ;
+        _previousZ = transform.parent.localEulerAngles.z;
+        
+        //Parent's position in world space
+        var targetPosition = transform.parent.position;
 
-        transform.Rotate(0, 0, -rotation.z);
+        //We rotate the camera around the parent's forward axis with -deltaZ
+        //This effectively counters the rotation normally applied by the object hierarchy
+        //Since RotateAround works in world space, we convert the parent's forward to world space
+        transform.RotateAround(targetPosition, transform.parent.TransformDirection(Vector3.forward), -deltaZ);
     }
 }
