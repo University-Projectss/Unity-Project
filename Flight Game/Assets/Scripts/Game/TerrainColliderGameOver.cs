01601cd6ff5e6932259cbc5554fd8b608d2dbc3c
Qmpzlawasd <diaconustefan16@gmail.com>
2023-12-16 00:36:07 +0200

    Resolve built-in class name collision

--- Flight Game/Assets/Scripts/Game/TerrainColliderGameOver.cs
+++ Flight Game/Assets/Scripts/Game/TerrainColliderGameOver.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainCollider : MonoBehaviour
+public class TerrainColliderGameOver : MonoBehaviour
 {
     [SerializeField]
     private GameOver _gameOver;
