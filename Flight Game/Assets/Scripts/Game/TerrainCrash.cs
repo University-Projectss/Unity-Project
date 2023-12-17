9bd1cb289faad9cdfea2d44d84186d97ce028671
Qmpzlawasd <diaconustefan16@gmail.com>
2023-12-17 10:39:20 +0200

    Improve collision detection

--- Flight Game/Assets/Scripts/Game/TerrainCrash.cs
+++ Flight Game/Assets/Scripts/Game/TerrainCrash.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TerrainCrash : MonoBehaviour
@@ -5,9 +6,19 @@ public class TerrainCrash : MonoBehaviour
     [SerializeField]
     private GameOver _gameOver;
 
+    [SerializeField]
+    private float _collisionLimit;
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag(Constants.TerrainTag))
+        if (!collision.gameObject.CompareTag(Constants.TerrainTag))
+        {
+            return;
+        }
+
+        Vector3 collisionDir = collision.contacts[0].point - transform.position;
+        float inlineSpeed = Vector3.Dot(collisionDir.normalized, GetComponent<Rigidbody>().velocity);
+        if (Math.Abs(inlineSpeed) < _collisionLimit)
         {
             _gameOver.ShowGameOverCrash();
         }
