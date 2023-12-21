6ed443fc2b2cfffd87cbb0bde08c97d47cb64bd4
Andrei Tava <tavaandrei@gmail.com>
2023-12-21 10:34:21 +0200

    Fix projectile pool bug

--- Flight Game/Assets/Scripts/Combat/ObjectPool.cs
+++ Flight Game/Assets/Scripts/Combat/ObjectPool.cs
@@ -10,25 +10,33 @@ public class ObjectPool : MonoBehaviour
 
     public int poolSize;
 
+    private int _validIndex;
+
     void Start()
     {
         _pool = new List<GameObject>();
         InitializeObjects();
+        _validIndex = 0;
     }
 
     public GameObject GetPooledObject()
     {
-        foreach(GameObject obj in _pool)
+
+        var obj = _pool[_validIndex];
+
+        for(int i = _validIndex + 1; i != _validIndex; i = ++i % poolSize)
         {
-            if (!obj.activeInHierarchy)
+            if (!_pool[i].activeInHierarchy)
             {
+                _validIndex = i;
                 obj.SetActive(true);
                 return obj;
             }
         }
-        int index = ResizePool();
-        _pool[index].SetActive(true);
-        return _pool[index];
+
+        _validIndex = ResizePool();
+        obj.SetActive(true);
+        return obj;
     }
 
     int ResizePool()
