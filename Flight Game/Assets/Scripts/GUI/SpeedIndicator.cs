2b2abc417f6f5489c450dc270df7e132720fb700
Diaconu Ștefan <69342294+Qmpzlawasd@users.noreply.github.com>
2024-01-03 16:39:43 +0200

    Add speed indicator (#78)
    * Add speed indicator
    
    * Use outlined font
    
    * Refactor variable name

---
+++ Flight Game/Assets/Scripts/GUI/SpeedIndicator.cs
@@ -0,0 +1,48 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class SpeedIndicator : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _plane;
+
+    [SerializeField]
+    private RectTransform _crosshair;
+
+    [SerializeField]
+    private TextMeshProUGUI _speedText;
+
+    [SerializeField]
+    private Color _colorFast;
+
+    [SerializeField]
+    private Color _colorMedium;
+
+    [SerializeField]
+    private Color _colorSlow;
+
+    [SerializeField]
+    private float _xOffset;
+
+    void Update()
+    {
+        _speedText.transform.localPosition = _crosshair.localPosition + Vector3.left * _xOffset;
+
+        float speed = _plane.velocity.magnitude;
+        if (speed > 37)
+        {
+            _speedText.color = _colorFast;
+        }
+        else if (speed < 24)
+        {
+            _speedText.color = _colorSlow;
+        }
+        else
+        {
+            _speedText.color = _colorMedium;
+        }
+        _speedText.text = Math.Floor(_plane.velocity.magnitude).ToString();
+    }
+}
+
